f19624c58d6f828a3f10f869f19ac9a341eafe13
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-10-18 16:52:20 +0600

    basic page added

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,19 @@
+const HomePage = () => {
+    return ( 
+        <div className="flex flex-col justify-between items-center w-full bg-gradient-to-tl from-blue-600 to-blue-950 ">
+            <div className="flex flex-col justify-center items-center w-full pb-2 my-8 max-w-2xl">
+                <p className="font-light text-lg">Tech Meets Marketing</p>
+                <p className="font-bold text-2xl">Bart Warrot Professional Service</p>
+                <p className="text-sm text-center">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual</p>
+                <button className="text-base p-2 rounded-lg ">
+                    Book an Consultancy
+                </button>
+            
+            </div>
+            
+            
+        </div>
+     );
+}
+ 
+export default HomePage;
